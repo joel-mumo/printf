4cbc9315a64e52f99328471c34d07b57ecd86fd2
elcodo254 <ellawachaga@gmail.com>
2022-11-23 17:05:49 -0800

    modified main.h with prototype for printf and struct op

--- main.h
+++ main.h
@@ -1,4 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include<stdio.h>
+#include<stdarg.h>
+
+/**
+ * struct op - struct op
+ *
+ * @spec: specifier
+ * @f: function associated
+ */
+typedef struct op
+{
+	char *spec;
+	int (*f)(va_list);
+} op_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
 #endif/*MAIN_H*/
