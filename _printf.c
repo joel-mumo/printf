1b774815447ecdbef3aaf6d2e0635f5a7904172d
elcodo254 <ellawachaga@gmail.com>
2022-11-27 18:28:05 -0800

    clean code

--- _printf.c
+++ _printf.c
@@ -1,7 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
 
 /**
  * _printf - creates printf function
--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <limits.h>
+#include <stdlib.h>
 
 /**
  * struct op - struct op
